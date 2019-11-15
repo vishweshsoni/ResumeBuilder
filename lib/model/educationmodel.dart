4339b6204d3a9f537274178d8b65a1cb7d488ae0
vishweshsoni <vishweshsoni@gmail.com>
2019-11-15 23:47:35 +0530

    Education details

---
+++ lib/model/educationmodel.dart
@@ -0,0 +1,62 @@
+//{
+//  "error": false,
+//  "message": "success",
+//  "data": {
+//        "id": 3,
+//        "degree": "BSc Ele",
+//        "university": "Gujarat University",
+//        "year": "2017",
+//        "cpi": "6.9"
+//    }
+//}
+
+
+class EdModel {
+  var id;
+    var degree;
+  var university;
+  var year;
+  var cpi;
+
+  EdModel({
+    this.id,
+    this.degree,
+    this.university,
+    this.year,
+    this.cpi,
+  });
+  factory EdModel.fromJson(Map<String, dynamic> json) {
+    return EdModel(
+      id: json['id'],
+      degree: json['degree'],
+      university: json['university'],
+      year: json['year'],
+        cpi:json['cpi']
+
+    );
+  }
+
+
+}
+class Users {
+  int id;
+  String name;
+  String username;
+  String email;
+
+  Users({
+    this.id,
+    this.name,
+    this.username,
+    this.email,
+  });
+
+  factory Users.fromJson(Map<String, dynamic> json) {
+    return Users(
+      id: json['id'],
+      name: json['name'],
+      email: json['email'],
+      username: json['username'],
+    );
+  }
+}
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:resume_app/data/user.dart';
+import 'package:resume_app/model/user.dart';
 import 'homepage.dart';
 import 'package:http/http.dart' as http;
 import 'package:dio/dio.dart';
