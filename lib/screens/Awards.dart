a5dd5aaac7e3da3a3d2a1528a8b3477a8898c426
Vidisha Shah <shahvidisha1996@gmail.com>
2019-11-22 13:35:35 +0530

    API integration in awards,experience and interest

--- lib/screens/Awards.dart
+++ lib/screens/Awards.dart
@@ -1,14 +1,97 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:resume_app/sizeconfig.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'homepage.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Awards extends StatefulWidget {
   @override
   _AwardsState createState() => _AwardsState();
 }
 
+
+
 class _AwardsState extends State<Awards> {
 
-  TextEditingController awards = new TextEditingController();
+  SharedPreferences prefs;
+  var u_id;
+  String uid="";
+
+  TextEditingController controllerAwards = new TextEditingController();
+
+  getdata() async{
+    prefs = await SharedPreferences.getInstance();
+    u_id = prefs.getString("u_id");
+    uid = u_id.toString();
+    print(u_id);
+    _getAwardDetails();
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    getdata();
+    super.initState();
+  }
+
+  Future<String>_getAwardDetails() async
+  {
+    print("hello");
+    print(uid);
+    String url = 'http://resume-builder1.herokuapp.com/resume/getAwards/'+uid;
+    print(url);
+    Map<String,String> headers = {"Content-type": "application/json"};
+
+    final data = await http.get(Uri.encodeFull(url),headers: headers);
+    var jsonData1 = json.decode(data.body.toString());
+    String awards = jsonData1["data"][0]["awards"];
+
+
+    controllerAwards =  TextEditingController(text: awards);
+
+
+    print("experienceDetails");
+    print(jsonData1);
+
+  }
+
+
+  Future<String> _addAwards(String awards) async
+  {
+    String url = 'http://resume-builder1.herokuapp.com/resume/addAwards/'+uid;
+    print(url);
+    Map<String,String> headers = {"Content-type": "application/json"};
+
+    final response = await http.post(Uri.encodeFull(url),
+        headers: headers,
+        body: json.encode({"resume_id":uid,"exp": awards}));
+    print(response.body.toString() + "qwerty");
+
+    String ans = response.body.toString();
+
+    var responseJson = jsonDecode(ans);
+
+    var result = responseJson["error"];
+
+    if(result==false)
+    {
+      Fluttertoast.showToast(
+          msg: "Added Details",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIos: 1,
+          backgroundColor: Colors.grey,
+          textColor: Colors.black87,
+          fontSize: 16.0
+      );
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+    }
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +114,9 @@ class _AwardsState extends State<Awards> {
             icon: const Icon(Icons.check),
             iconSize: 30.0,
             color: Colors.white,
-            onPressed: (){},
+            onPressed: (){
+              _addAwards(controllerAwards.text);
+            },
           ),
         ],
       ),
@@ -50,7 +135,8 @@ class _AwardsState extends State<Awards> {
                 child: Column(
                   children: <Widget>[
                     TextField(
-                      controller: awards,
+                      autofocus: true,
+                      controller: controllerAwards,
                       maxLines: 10,
                       decoration: InputDecoration(
                         alignLabelWithHint: true,
--- lib/screens/interest.dart
+++ lib/screens/interest.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:resume_app/sizeconfig.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'homepage.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 
 class Interest extends StatefulWidget {
   @override
@@ -8,7 +14,79 @@ class Interest extends StatefulWidget {
 
 class _InterestState extends State<Interest> {
 
-  TextEditingController experience = new TextEditingController();
+  SharedPreferences prefs;
+  var u_id;
+  String uid="";
+
+  TextEditingController controllerInterest = new TextEditingController();
+
+  getdata() async{
+    prefs = await SharedPreferences.getInstance();
+    u_id = prefs.getString("u_id");
+    uid = u_id.toString();
+    print(u_id);
+    _getInterestDetails();
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    getdata();
+    super.initState();
+  }
+
+  Future<String>_getInterestDetails() async
+  {
+    print("hello");
+    print(uid);
+    String url = 'http://resume-builder1.herokuapp.com/resume/getInterests/'+uid;
+    print(url);
+    Map<String,String> headers = {"Content-type": "application/json"};
+
+    final data = await http.get(Uri.encodeFull(url),headers: headers);
+    var jsonData1 = json.decode(data.body.toString());
+    String awards = jsonData1["data"][0]["interest"];
+
+    controllerInterest =  TextEditingController(text: awards);
+
+    print("interestDetails");
+    print(jsonData1);
+
+  }
+
+  Future<String> _addInterest(String interest) async
+  {
+    String url = 'http://resume-builder1.herokuapp.com/resume/addInterests/'+uid;
+    print(url);
+    Map<String,String> headers = {"Content-type": "application/json"};
+
+    final response = await http.post(Uri.encodeFull(url),
+        headers: headers,
+        body: json.encode({"resume_id":uid,"interest": interest}));
+    print(response.body.toString() + "qwerty");
+
+    String ans = response.body.toString();
+
+    var responseJson = jsonDecode(ans);
+
+    var result = responseJson["error"];
+
+    if(result==false)
+    {
+      Fluttertoast.showToast(
+          msg: "Added Details",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIos: 1,
+          backgroundColor: Colors.grey,
+          textColor: Colors.black87,
+          fontSize: 16.0
+      );
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+    }
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +110,9 @@ class _InterestState extends State<Interest> {
             icon: const Icon(Icons.check),
             iconSize: 30.0,
             color: Colors.white,
-            onPressed: (){},
+            onPressed: (){
+              _addInterest(controllerInterest.text);
+            },
           ),
         ],
       ),
@@ -51,7 +131,7 @@ class _InterestState extends State<Interest> {
                 child: Column(
                   children: <Widget>[
                     TextField(
-                      controller: experience,
+                      controller: controllerInterest,
                       maxLines: 10,
                       decoration: InputDecoration(
                         alignLabelWithHint: true,
